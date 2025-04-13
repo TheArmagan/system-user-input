5f24995142ed0be28a22e92c2261f841a3003919
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2025-04-13 23:10:41 +0300

    a

--- build.js
+++ build.js
@@ -1,23 +1,14 @@
-const { build } = require("esbuild");
 const fs = require("fs");
+const cp = require("child_process");
 const path = require("path");
 
-(async () => {
-  console.time("Build time");
-  await build({
-    entryPoints: ["src/index.ts"],
-    bundle: true,
-    outfile: "dist/index.js",
-    sourcemap: "external",
-    minify: true,
-    platform: "node",
-  });
-  console.timeEnd("Build time");
+console.time("Build time");
+cp.execSync("pnpm run tsc", { stdio: "inherit" });
+console.timeEnd("Build time");
 
-  fs.readdirSync("./bin").forEach((file) => {
-    fs.copyFileSync(path.join("./bin", file), path.join("./dist", file));
-    console.log(`Copied ${file} to dist`);
-  });
+fs.readdirSync("./bin").forEach((file) => {
+  fs.copyFileSync(path.join("./bin", file), path.join("./dist", file));
+  console.log(`Copied ${file} to dist`);
+});
 
-  console.log("Build complete");
-})();
+console.log("Build complete");
